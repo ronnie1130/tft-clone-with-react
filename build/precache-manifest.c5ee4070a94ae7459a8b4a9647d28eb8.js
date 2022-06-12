a39d6af6485a7df78845fd5b1e2d90f2688ae724
ronnie1130 <84077671+ronnie1130@users.noreply.github.com>
2022-06-12 21:38:32 +0800

    npm build

---
+++ build/precache-manifest.c5ee4070a94ae7459a8b4a9647d28eb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d8fe2405a581960601a",
+    "url": "/tft-clone-with-react/static/css/main.29027eab.chunk.css"
+  },
+  {
+    "revision": "3d8fe2405a581960601a",
+    "url": "/tft-clone-with-react/static/js/main.3d8fe240.chunk.js"
+  },
+  {
+    "revision": "5016ccde18b6b2b6d682",
+    "url": "/tft-clone-with-react/static/css/1.135568f8.chunk.css"
+  },
+  {
+    "revision": "5016ccde18b6b2b6d682",
+    "url": "/tft-clone-with-react/static/js/1.5016ccde.chunk.js"
+  },
+  {
+    "revision": "40cf48985202910000df",
+    "url": "/tft-clone-with-react/static/js/runtime~main.40cf4898.js"
+  },
+  {
+    "revision": "c1c63ea12978791e0e0fd3de97c282a5",
+    "url": "/tft-clone-with-react/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/tft-clone-with-react/precache-manifest.c5ee4070a94ae7459a8b4a9647d28eb8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/tft-clone-with-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
